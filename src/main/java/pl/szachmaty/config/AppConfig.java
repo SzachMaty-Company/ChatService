35cc1608a0421aaedd7c162273d3fede63fb6064
bartosz <bartosz.socki@gmail.com>
2023-11-24 13:21:46 +0100

    added cors config

---
+++ src/main/java/pl/szachmaty/config/AppConfig.java
@@ -0,0 +1,23 @@
+package pl.szachmaty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+
+    final List<String> corsAllowedMethods = List.of(HttpMethod.GET.name(), HttpMethod.POST.name());
+    final List<String> corsAllowedOriginPatterns = List.of("*");
+
+    CorsConfiguration corsConfiguration() {
+        var config = new CorsConfiguration();
+        config.setAllowedMethods(corsAllowedMethods);
+        config.setAllowedOriginPatterns(corsAllowedOriginPatterns);
+
+        return config;
+    }
+
+}
