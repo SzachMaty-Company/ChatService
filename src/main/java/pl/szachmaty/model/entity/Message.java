ba081f5820aee2fed492ca5d3f6f5356920aae1a
bartosz <bartosz.socki@gmail.com>
2023-11-26 13:16:20 +0100

    added sender to message and added message repository test

--- src/main/java/pl/szachmaty/model/entity/Message.java
+++ src/main/java/pl/szachmaty/model/entity/Message.java
@@ -1,13 +1,15 @@
 package pl.szachmaty.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "message", schema = "chat_service")
 public class Message {
@@ -19,5 +21,9 @@ public class Message {
     @Column(name = "`timestamp`")
     private LocalDateTime timestamp;
     private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "sender_id")
+    private User sender;
 
 }
