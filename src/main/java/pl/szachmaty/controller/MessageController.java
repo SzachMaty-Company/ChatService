4c5c54a604c34ab1b06adc64406e9e9b6d37a6a6
bartosz <bartosz.socki@gmail.com>
2023-11-26 20:12:33 +0100

    solved cors, added missing timestamp annotation and updated web client

--- src/main/java/pl/szachmaty/controller/MessageController.java
+++ src/main/java/pl/szachmaty/controller/MessageController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import pl.szachmaty.service.MessageQueryService;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin
 public class MessageController {
 
     MessageQueryService messageQueryService;
--- src/main/java/pl/szachmaty/model/entity/Message.java
+++ src/main/java/pl/szachmaty/model/entity/Message.java
@@ -2,6 +2,7 @@ package pl.szachmaty.model.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -19,7 +20,9 @@ public class Message {
     private Long id;
 
     @Column(name = "`timestamp`")
+    @CreationTimestamp
     private LocalDateTime timestamp;
+
     private String message;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
