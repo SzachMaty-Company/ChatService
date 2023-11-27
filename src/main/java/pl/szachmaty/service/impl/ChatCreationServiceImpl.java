f1e4f73ad50a91f66a0654ac16b05d709836a8b1
bartosz <bartosz.socki@gmail.com>
2023-11-27 20:04:41 +0100

    implemented chat creation service

--- src/main/java/pl/szachmaty/service/impl/ChatCreationServiceImpl.java
+++ src/main/java/pl/szachmaty/service/impl/ChatCreationServiceImpl.java
@@ -1,14 +1,33 @@
 package pl.szachmaty.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.szachmaty.model.entity.Chat;
+import pl.szachmaty.model.repository.ChatRepository;
+import pl.szachmaty.model.repository.UserRepository;
 import pl.szachmaty.service.ChatCreationService;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @Service
 public class ChatCreationServiceImpl implements ChatCreationService {
+
+    private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+
     @Override
     public Long createChat(Set<Long> chatMembersId) {
-        return null;
+        var chat = new Chat();
+        var members = chatMembersId.stream()
+                .map(userRepository::getReferenceById)
+                .collect(Collectors.toSet());
+
+        chat.setChatMembers(members);
+
+        var savedChat = chatRepository.save(chat);
+        return savedChat.getId();
     }
+
 }
