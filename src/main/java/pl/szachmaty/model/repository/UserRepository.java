d61bcbd61ef27f796b7678e4ca459995b6996fc4
bartosz <bartosz.socki@gmail.com>
2024-02-25 00:39:01 +0100

    refactor: added query by global user id

--- src/main/java/pl/szachmaty/model/repository/UserRepository.java
+++ src/main/java/pl/szachmaty/model/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package pl.szachmaty.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.szachmaty.model.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.globalUserId.id = :id")
+    User findUserByGlobalUserId(String id);
+
 }
