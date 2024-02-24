fbb63660fe07bd738e169447d9c1112aafb100fc
bartosz <bartosz.socki@gmail.com>
2024-02-25 00:41:07 +0100

    feat: added interceptor for websocket authentication

--- src/main/java/pl/szachmaty/config/WebSocketConfig.java
+++ src/main/java/pl/szachmaty/config/WebSocketConfig.java
@@ -1,25 +1,50 @@
 package pl.szachmaty.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import pl.szachmaty.model.repository.UserRepository;
+import pl.szachmaty.security.AuthenticationChannelInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final UserRepository userRepository;
+
+    public WebSocketConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/registerws")
                 .setAllowedOriginPatterns("*");
     }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
 //        config.setApplicationDestinationPrefixes("/app", "/user");
-        config.setApplicationDestinationPrefixes("/app");
+        config.setApplicationDestinationPrefixes("/app", "/chat");
 //        config.enableSimpleBroker("/topic");
 //        config.enableSimpleBroker("/user");
 //        config.enableSimpleBroker("/user/**/messages");
 //        config.setUserDestinationPrefix("/user");
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authenticationChannelInterceptor());
+    }
+
+    @Bean
+    ChannelInterceptor authenticationChannelInterceptor() {
+        return new AuthenticationChannelInterceptor(userRepository);
+    }
+
 }
--- src/main/java/pl/szachmaty/security/UserAuthenticationToken.java
+++ src/main/java/pl/szachmaty/security/UserAuthenticationToken.java
@@ -1,20 +1,20 @@
 package pl.szachmaty.security;
 
-import org.apache.catalina.User;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import pl.szachmaty.model.entity.User;
 
 import java.util.List;
 
-public class UserIdAuthenticationToken extends AbstractAuthenticationToken {
+public class UserAuthenticationToken extends AbstractAuthenticationToken {
 
     public static final List<? extends GrantedAuthority> DEFAULT_AUTHORITIES = List.of(
             new SimpleGrantedAuthority("USER")
     );
     private User principal;
 
-    public UserIdAuthenticationToken(User principal) {
+    public UserAuthenticationToken(User principal) {
         super(DEFAULT_AUTHORITIES);
         this.principal = principal;
     }
