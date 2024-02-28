12dca1a681b2bc1ef8da8f9d60dee09fb3d9037b
bartosz <bartosz.socki@gmail.com>
2024-02-28 22:44:40 +0100

    feat: added custom args resolver for websockets

--- src/main/java/pl/szachmaty/config/WebSocketConfig.java
+++ src/main/java/pl/szachmaty/config/WebSocketConfig.java
@@ -2,33 +2,31 @@ package pl.szachmaty.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.user.SimpUserRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import pl.szachmaty.model.repository.UserRepository;
+import pl.szachmaty.security.annotation.StompAuthenticationPrincipalArgumentResolver;
 import pl.szachmaty.security.interceptor.AuthenticationChannelInterceptor;
-import pl.szachmaty.security.interceptor.MaxSessionChannelInterceptor;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final UserRepository userRepository;
-    private final SimpUserRegistry simpUserRegistry;
+    private final AuthenticationManager authenticationManager;
 
     @Value("${ws.max-sessions:3}")
     private int maxSessions;
 
-    public WebSocketConfig(UserRepository userRepository, @Lazy SimpUserRegistry simpUserRegistry) {
-        this.userRepository = userRepository;
-        this.simpUserRegistry = simpUserRegistry;
+    public WebSocketConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -47,17 +45,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(
-                authenticationChannelInterceptor(),
-                maxSessionChannelInterceptor()
+                new AuthenticationChannelInterceptor(authenticationManager)
         );
     }
 
-    ChannelInterceptor authenticationChannelInterceptor() {
-        return new AuthenticationChannelInterceptor(userRepository);
-    }
-
-    ChannelInterceptor maxSessionChannelInterceptor() {
-        return new MaxSessionChannelInterceptor(maxSessions, simpUserRegistry);
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.addAll(List.of(new StompAuthenticationPrincipalArgumentResolver()));
     }
 
 }
