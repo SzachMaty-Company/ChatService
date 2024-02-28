93e1808aa981270aa27919df0df3b580f2e1121d
bartosz <bartosz.socki@gmail.com>
2024-02-28 22:46:09 +0100

    fix: using custom principal annotation and added missing authorization

--- src/main/java/pl/szachmaty/controller/MessageController.java
+++ src/main/java/pl/szachmaty/controller/MessageController.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.szachmaty.model.dto.ChatMessageDto;
+import pl.szachmaty.model.entity.User;
 import pl.szachmaty.service.MessageQueryService;
+
+import java.security.Principal;
 
 @RestController
 @AllArgsConstructor
@@ -18,8 +23,10 @@ public class MessageController {
 
     final MessageQueryService messageQueryService;
 
-    // TODO: require authorization
     @GetMapping(path = "/chat/{chatId}/messages")
+    @PreAuthorize(
+            "@chatParticipantValidator.isUserChatMember(principal, #chatId)"
+    )
     ResponseEntity<Slice<ChatMessageDto>> queryMessages(@PathVariable Long chatId, Pageable pageable) {
         var messages = messageQueryService.queryMessages(chatId, pageable);
         return ResponseEntity.ok(messages);
