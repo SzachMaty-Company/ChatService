9e96765d6ad921192a8af6bed244c922cea9b229
bartosz <bartosz.socki@gmail.com>
2024-02-29 21:19:41 +0100

    fix: removed sort in controllers pageable

--- src/main/java/pl/szachmaty/controller/ChatController.java
+++ src/main/java/pl/szachmaty/controller/ChatController.java
@@ -1,26 +1,27 @@
 package pl.szachmaty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import pl.szachmaty.model.dto.*;
+import pl.szachmaty.model.dto.ChatCreationRequest;
+import pl.szachmaty.model.dto.ChatCreationResponse;
+import pl.szachmaty.model.dto.ChatListItem;
+import pl.szachmaty.model.dto.Message;
 import pl.szachmaty.model.entity.User;
 import pl.szachmaty.model.repository.ChatRepository;
-import pl.szachmaty.security.UserJwtAuthenticationToken;
 import pl.szachmaty.security.annotation.StompAuthenticationPrincipal;
 import pl.szachmaty.service.ChatCreationService;
 import pl.szachmaty.service.ChatListService;
 import pl.szachmaty.service.MessageSendingService;
-
-import java.security.Principal;
 
 @RestController
 @CrossOrigin
@@ -33,8 +34,9 @@ public class ChatController {
     final ChatRepository chatRepository;
 
     @GetMapping(path = "/user/chats")
-    ResponseEntity<Slice<ChatListItem>> getChatListForUser(@AuthenticationPrincipal User user, Pageable pageable) {
-        return ResponseEntity.ok(chatListService.getUserChats(user.getUserId().getId(), pageable));
+    ResponseEntity<Slice<ChatListItem>> getChatListForUser(@AuthenticationPrincipal User user, @ParameterObject Pageable pageable) {
+        var correctedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.unsorted());
+        return ResponseEntity.ok(chatListService.getUserChats(user.getUserId().getId(), correctedPageable));
     }
 
     @MessageMapping("/message")
@@ -43,6 +45,7 @@ public class ChatController {
     }
 
     @Profile({"dev", "local"})
+    @Operation(description = "for internal purposes only, don't use it")
     @PostMapping(path = "/chat")
     ResponseEntity<ChatCreationResponse> createChat(@RequestBody ChatCreationRequest chatCreationRequest) {
         var chat = chatCreationService.createChat(chatCreationRequest);
